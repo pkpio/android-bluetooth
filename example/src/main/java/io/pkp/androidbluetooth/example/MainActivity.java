89c8d18e4b5b0e1ae9cdfd6f110568b9a4139e4e
Praveen Kumar Pendyala <praveendath92@gmail.com>
2016-07-06 18:33:36 +0200

    Example app updated with logger listing of scan

--- example/src/main/java/io/pkp/androidbluetooth/example/MainActivity.java
+++ example/src/main/java/io/pkp/androidbluetooth/example/MainActivity.java
@@ -1,13 +1,47 @@
 package io.pkp.androidbluetooth.example;
 
+import android.bluetooth.BluetoothDevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import io.pkp.androidbluetooth.BluetoothSetup;
+
+public class MainActivity extends AppCompatActivity implements io.pkp.androidbluetooth.OnBTScanListener{
+    TextView testview;
+    List<BluetoothDevice> mBTDevices;
+    BluetoothSetup mBTSetup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testview = (TextView) findViewById(R.id.hello);
+        mBTSetup = new BluetoothSetup(this);
+    }
+
+    @Override
+    public void onDeviceFound(BluetoothDevice newDevice) {
+        Log.d("BT_DEVICE", newDevice.getName());
+    }
+
+    @Override
+    public void onScanComplete() {
+        Log.e("BT_DEVICE", "Scan complete!");
+    }
+
+    public void onPause(){
+        super.onPause();
+        mBTSetup.stopDiscovery();
+        Log.e("BT_DEVICE", "Scan stopped");
+    }
+
+    public void onResume(){
+        super.onResume();
+        mBTSetup.discoverDevices(this);
+        Log.e("BT_DEVICE", "Scan started");
     }
 }
